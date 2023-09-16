6aaf441427083f2140da3ea6817e1ac7ec6a9330
ShubhamTiwari479 <67504568+ShubhamTiwari479@users.noreply.github.com>
2023-09-16 16:07:34 +0530

    p

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 });
+
 const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
@@ -13,19 +14,19 @@ const nextConfig = {
       {
         protocol: process.env.NEXT_PUBLIC_PROTOCOL || 'https',
         hostname: process.env.NEXT_PUBLIC_API_HOSTNAME || 'example.com',
-        port: '',        // Add a valid port or set it to an empty string if not applicable
+        port: '3000',        // Add a valid port or set it to an empty string if not applicable
         pathname: '/imgs/**', // Add a valid pathname or set it to an empty string if not applicable
       },
       {
         protocol: process.env.NEXT_PUBLIC_PROTOCOL || 'https',
         hostname: process.env.NEXT_PUBLIC_STORAGE_HOSTNAME || 'example.com',
-        port: '',        // Add a valid port or set it to an empty string if not applicable
+        port: '3000',        // Add a valid port or set it to an empty string if not applicable
         pathname: '/imgs/**', // Add a valid pathname or set it to an empty string if not applicable
       },
       {
         protocol: process.env.NEXT_PUBLIC_PROTOCOL || 'https',
         hostname: 'demo-api.foodyman.org',
-        port: '',        // Add a valid port or set it to an empty string if not applicable
+        port: '3000',        // Add a valid port or set it to an empty string if not applicable
         pathname: '/imgs/**', // Add a valid pathname or set it to an empty string if not applicable
       },
     ],
